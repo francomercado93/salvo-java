4207fad048c241bb46135bbc85bbd2c7e79d012d
francomercado93 <mercadofranco.a@gmail.com>
2019-10-21 09:22:24 -0300

    Fixs

--- src/main/java/com/codeoftheweb/salvo/SalvoController.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -27,18 +25,27 @@ public class SalvoController {
     //ids de juegos
     @RequestMapping("/games-ids")
     public List<Long> getAllGamesIds() {
-        return gameRepository.findAll().stream().map(game -> game.getId()).collect(Collectors.toList());
+        return gameRepository.findAll()
+                .stream()
+                .map(game -> game.getId())
+                .collect(Collectors.toList());
     }
 
     //Obtenemos un json con todos los juegos con un formato que elegimos
     @RequestMapping("/games")
     public List<Object> getAllGames() {
-        return gameRepository.findAll().stream().map(owner -> owner.makeOwnerDTOGames()).collect(Collectors.toList());
+        return gameRepository.findAll()
+                .stream()
+                .map(owner -> owner.makeOwnerDTOGames())
+                .collect(Collectors.toList());
     }
 
     @RequestMapping("/players")
     public List<Object> getAllPlayers() {
-        return playerRepository.findAll().stream().map(owner -> owner.makeOwnerDTOPlayers()).collect(Collectors.toList());
+        return playerRepository.findAll()
+                .stream()
+                .map(owner -> owner.makeOwnerDTOPlayers())
+                .collect(Collectors.toList());
     }
 
     //Le paso el id de gamePlayer y en el json el primer id es el del game
@@ -46,18 +53,25 @@ public class SalvoController {
     public Map<String, Object> getGamePlayerInformation(@PathVariable Long nn) {
         GamePlayer gamePlayer = gamePlayerRepository.findById(nn).get();
         Game game = gamePlayer.getGame();
-        //Reutilizo el DTO del game y le agrego los barcos del player
+        //Reutilizo el DTO del game y le agrego los barcos del player y  los salvos
         Map<String, Object> dto = game.makeOwnerDTOGames();
-        dto.put("ships", gamePlayer.getShips()
-                .stream()
-                .map(ship -> ship.makeDTOShip())
-                .collect(Collectors.toList()));
+        putShips(gamePlayer, dto);
+        putSalvoes(game, dto);
+        return dto;
+    }
 
+    private void putSalvoes(Game game, Map<String, Object> dto) {
         dto.put("salvoes", game.getGamePlayers()
                 .stream()
                 .flatMap(gp -> gp.getSalvoes()
                         .stream()
                         .map(salvo -> salvo.makeDTOSalvo())));
-        return dto;
+    }
+
+    private void putShips(GamePlayer gamePlayer, Map<String, Object> dto) {
+        dto.put("ships", gamePlayer.getShips()
+                .stream()
+                .map(ship -> ship.makeDTOShip())
+                .collect(Collectors.toList()));
     }
 }
